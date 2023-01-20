b9c8cd33831ccb979bed1fe9798da82f3c9de6e7
jesseelson <111322240+jesseelson@users.noreply.github.com>
2023-01-19 20:26:59 -0600

    Update scraper.rb

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'json'
 
 class Scraper
     doc = Nokogiri::HTML(URI.open("https://en.wikipedia.org/wiki/List_of_best-selling_video_games"))
-
+    video_game_row = doc.css("tbody li")
     binding.pry
 end
 
